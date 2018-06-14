2733b91810f6c1857ee4e4dbd4216dab5f3993b9
jannunzi <jannunzi@gmail.com>
2018-06-14 19:56:48 -0400

    added form editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
-import {OmdapiServiceClientJs} from "./services/omdapi.service.client.js";
+// import {OmdapiServiceClientJs} from "./services/omdapi.service.client.js";
 import {FormsModule} from "@angular/forms";
 import {MovieServiceClient} from "./services/movie.service.client";
+import { FormEditorComponent } from './form-editor/form-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieSearchComponent
+    MovieSearchComponent,
+    FormEditorComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [
-    OmdapiServiceClientJs,
+    // OmdapiServiceClientJs,
     MovieServiceClient
   ],
   bootstrap: [AppComponent]
