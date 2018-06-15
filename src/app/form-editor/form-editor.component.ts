ceadfe8b404195fff9af920601513beab2d4496c
jannunzi <jannunzi@gmail.com>
2018-06-14 20:01:52 -0400

    added button to add form elements

--- src/app/form-editor/form-editor.component.ts
+++ src/app/form-editor/form-editor.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormEditorComponent implements OnInit {
 
-  inputType = '';
+  inputType = 'text';
   constructor() { }
+
+  addFormElement(inputType) {
+    console.log(inputType);
+  }
 
   ngOnInit() {
   }
